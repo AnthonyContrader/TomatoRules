4d9bd2229f9d1880e458461fae3f7716a070b6bf
daniele.adelfio <daniele.adelfio@gmail.com>
2020-05-22 13:05:19 +0200

    Added Category - Tool Relationship

--- src/app/admin/categories/categories.component.ts
+++ src/app/admin/categories/categories.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService} from 'src/service/category.service';
 import { CategoryDTO} from 'src/dto/categorydto';
+import { ToolDTO } from 'src/dto/tooldto';
+import { ToolService } from 'src/service/tool.service';
+
 
 @Component({
   selector: 'app-categories',
@@ -11,15 +14,22 @@ export class CategoriesComponent implements OnInit {
 
   categories: CategoryDTO[];
   categorytoinsert: CategoryDTO = new CategoryDTO();
+  tools: ToolDTO[];
 
-  constructor(private service: CategoryService) { }
+  constructor(private service: CategoryService, private serviceTool: ToolService) { }
 
   ngOnInit() {
     this.getCategories();
+    this.getTools();
+  }
+
+  getTools() {
+    this.serviceTool.getAll().subscribe(tools => this.tools = tools);
   }
 
   getCategories() {
     this.service.getAll().subscribe(categories => this.categories = categories);
+    this.serviceTool.getAll().subscribe(tools => this.tools);
   }
 
   delete(category: CategoryDTO) {
--- src/dto/categorydto.ts
+++ src/dto/categorydto.ts
@@ -1,3 +1,6 @@
+import { RootContextFlags } from '@angular/core/src/render3/interfaces/view';
+import { ToolDTO } from './tooldto';
+
 /**
  * Classe DTO di User. DEVE essere uguale (stesso nome classe, stessi attributi e stessi nomi) a
  * quello nel backend. 
@@ -7,6 +10,8 @@
  * @author Vittorio Valent
  */
 export class CategoryDTO {
+
+    tool: ToolDTO;
 
     id: number;
 
