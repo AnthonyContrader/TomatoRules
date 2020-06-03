b402ca78c5b5b6cd2b542c2ac6d92628fa49bd09
daniele.adelfio <daniele.adelfio@gmail.com>
2020-06-03 12:29:22 +0200

    Added Gateway

---
+++ gateway/src/main/java/com/contrader/tomatorulesmicroservices/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.contrader.tomatorulesmicroservices.service.dto;
---
+++ gateway/src/main/java/com/contrader/tomatorulesmicroservices/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.contrader.tomatorulesmicroservices.web.rest;
